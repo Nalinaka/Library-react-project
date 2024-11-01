fd8ed5f6286227e8ac5ae0566e808c0871a04c7f
Nalinaka <nalinaka@gmail.com>
2024-11-01 11:27:32 -0700

    Completed library

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,31 +1,64 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import Price from "./ui/Price";
-import Ratings from "./ui/Ratings";
+import Price from "./Price";
+import Ratings from "./Ratings";
 
 const Book = ({ book }) => {
-  // const [img, setImg] = useState();
+  const [img, setImg] = useState();
+
+
+  const mountedRef = useRef(true);
+
+
+  // This loads image from JavaScript
+  useEffect(() => {
+    const image = new Image();
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout(() => {
+      if (mountedRef.current) {
+        setImg(image);
+      }
+      }, 300);
+    };
+    return () => {
+  // When the component unmounts
+    mountedRef.current = false; 
+    }
+
+  })
+  // This skeleton code gives it that gray loading state, we use onLoad
   return (
     <div className="book">
-      <Link to={`/books/${book.id}`}>
+      {!img ? (
+            <>
+        <Link to={`/books/${book.id}`}>
         <figure className="book__img--wrapper">
-          <img src={book.url} alt="" className="book__img" />
+          <img src={img.src} alt="" className="book__img"/>
         </figure>
       </Link>
       <div clLinkssName="book__title">
         <Link to={`/books/${book.id}`} className="book__title--link">
-        {book.title}
+          {book.title}
         </Link>
       </div>
       <Rating rating={book.rating} />
-      <Price salePrice={book.salePrice} originalPrice={book.originalPrice}/>
-    </div>
+      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+              <div className="book__img--skeleton"></div>
+              <div className="skeleton book__title--skeleton"></div>
+              <div className="skeleton book__rating--skeleton"></div>
+              <div className="skeleton book__price--skeleton"></div>
+            </> 
+            </div>
+            </div>
+          // ) : (
   );
-}
+};
 
 export default Book;
 
-// When we switch routes dont set image to unmounted component
+{/* // When we switch routes dont set image to unmounted component
 //   const mountedRef = useRef(true);
 
 //   useEffect(() => {
@@ -74,4 +107,4 @@ export default Book;
 //       )}
 //     </div>
 //   );
-// };*/
+// };*/ }
