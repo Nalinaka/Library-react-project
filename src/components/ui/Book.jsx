50fda9f2830dcaf4b11e111fba69d0bac67ab5e3
Nalinaka <nalinaka@gmail.com>
2024-11-04 11:21:58 -0800

    Fixed all issues and ready to deploy

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -2,10 +2,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import Price from "./Price";
-import Ratings from "./Ratings";
+import Rating from "./Rating";
 
 const Book = ({ book }) => {
-  const [img, setImg] = useState();
+  const [img, setImg] = useState(null);
 
 
   const mountedRef = useRef(true);
@@ -38,21 +38,23 @@ const Book = ({ book }) => {
           <img src={img.src} alt="" className="book__img"/>
         </figure>
       </Link>
-      <div clLinkssName="book__title">
+      <div className="book__title">
         <Link to={`/books/${book.id}`} className="book__title--link">
           {book.title}
         </Link>
       </div>
       <Rating rating={book.rating} />
       <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+      </>
+      ) : (
+        <>
               <div className="book__img--skeleton"></div>
               <div className="skeleton book__title--skeleton"></div>
               <div className="skeleton book__rating--skeleton"></div>
               <div className="skeleton book__price--skeleton"></div>
             </> 
-            </div>
-            </div>
-          // ) : (
+      )}
+      </div>
   );
 };
 
